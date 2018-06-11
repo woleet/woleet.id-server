7174ed0947517899203aa34303fd6c77f628951e
Maxime Tissier <maxime.tissier@gmail.com>
2018-06-11 17:36:17 +0200

    Added nav & router

---
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {BreakpointObserver, Breakpoints, BreakpointState} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
+
+}
