3f6808d167b6836f16fe147b4d65086443c73b44
Maxime Tissier <maxime.tissier@gmail.com>
2018-06-12 14:17:40 +0200

    Expand side nav on hover

--- client/src/app/nav-bar/nav-bar.ts
+++ client/src/app/nav-bar/nav-bar.ts
@@ -10,12 +10,6 @@ import {map} from 'rxjs/operators';
 })
 export class NavBarComponent {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches)
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {
-  }
+  constructor() {  }
 
 }
