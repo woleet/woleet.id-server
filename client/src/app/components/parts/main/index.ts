95780a9ea57e3b8aa12d47f45f8f2225c3197ea1
Maxime Tissier <maxime.tissier@gmail.com>
2018-08-27 16:41:43 +0200

    Added page data service

--- client/src/app/components/parts/main/index.ts
+++ client/src/app/components/parts/main/index.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { PageDataService } from '@services/page-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './index.html',
   styleUrls: ['./style.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private titleService: PageDataService) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map((route) => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+
+        return route;
+      }),
+      filter((route) => route.outlet === 'primary'),
+      mergeMap((route) => route.data),
+    ).subscribe((event) => this.titleService.setTitle(event['title']));
+  }
+
 }
