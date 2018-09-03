21573e069be2651d9d4f1bca2aebf016f08a8ef9
Maxime Tissier <maxime.tissier@gmail.com>
2018-09-03 14:47:05 +0200

    Added "loglevel" package in both client and server

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -1,3 +1,9 @@
+import * as log from 'loglevel';
+
+import { environment } from '@env/environment';
+
 const mainRoute = '/user';
+
+log.setLevel(environment.production ? 'warn' : 'debug');
 
 export { mainRoute };
