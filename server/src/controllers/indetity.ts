3be49a70abc92183d8dd1cadca9add253a26871a
Maxime Tissier <maxime.tissier@gmail.com>
2018-09-17 16:26:44 +0200

    WOLEET-664: Fix security issue with identity URL

--- server/src/controllers/indetity.ts
+++ server/src/controllers/indetity.ts
@@ -1,9 +1,10 @@
 import * as crypto from 'crypto';
-import { Key, User } from '../database';
+import { Key } from '../database';
 import { NotFoundKeyError, BlockedKeyError } from '../errors';
 
 import * as message from 'bitcoinjs-message';
 import { serializeIdentity } from '../api/serialize/identity';
+import { getServerConfig } from './server-config';
 
 export async function getIdentity(leftData: string, pubKey: string) {
 
@@ -20,7 +21,7 @@ export async function getIdentity(leftData: string, pubKey: string) {
 
   const identity = key.getDataValue('user');
 
-  const rightData = crypto.randomBytes(32).toString('hex');
+  const rightData = getServerConfig().identityUrl + '.' + crypto.randomBytes(16).toString('hex');
 
   const sig = message.sign(leftData + rightData, Buffer.from(key.getDataValue('privateKey'), 'hex'));
 
