5afc7597267b30de916570c84658ce5b6704ee2a
Maxime Tissier <maxime.tissier@gmail.com>
2018-10-17 12:00:55 +0200

    Fixed ts tests initialization

---
+++ server/test/before.ts
@@ -0,0 +1,11 @@
+/* tslint-env mocha */
+/* tslint prefer-arrow-callback: "off" */
+/* tslint import/no-extraneous-dependencies: "off" */
+
+import 'mocha';
+import { setSecret } from '../../server/src/controllers/utils/encryption';
+
+before(() => {
+  console.log('Initializing test secret...');
+  setSecret('test');
+})
