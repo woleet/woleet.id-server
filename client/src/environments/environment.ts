fbe95b16f341f0d4bc990632342556ca581d6f1c
vbarat <vincent.barat@woleet.com>
2018-12-12 16:57:40 +0100

    Version 0.3.1: test suite fixed

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,14 +1,12 @@
-// The content of the specified environment will overwrite this one during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The build system defaults to this environment, but if you do
+// `ng build|serve --prod` then `environment.prod.ts` will be used instead.
 
 const { protocol, hostname } = window.location;
 
 const port = 3000;
 
 export const environment = {
-  version: '0.3.0',
+  version: '0.3.1',
   production: false,
   serverURL: `${protocol}//${hostname}:${port}`
 };
