82a26d0ed3074c49d88c763749f70eef1be1f319
Maxime Tissier <maxime.tissier@gmail.com>
2019-02-07 10:56:13 +0100

    Fixed production build error

--- client/src/app/components/parts/intl-tel-input/index.ts
+++ client/src/app/components/parts/intl-tel-input/index.ts
@@ -1,5 +1,5 @@
-import { Component, Input, ContentChild, ViewChild, OnInit, Output, EventEmitter} from '@angular/core';
-import { FormControl, ValidationErrors, AbstractControl} from '@angular/forms';
+import { Component, Input, ContentChild, ViewChild, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, ValidationErrors, AbstractControl } from '@angular/forms';
 import { parsePhoneNumberFromString, AsYouType } from 'libphonenumber-js';
 import { ErrorMessageProvider } from '@components/util';
 import * as log from 'loglevel';
@@ -7,8 +7,8 @@ import * as log from 'loglevel';
 function phoneNumberValidator(control: AbstractControl): ValidationErrors | null {
   if (control.value !== undefined && control.value !== '') {
     if (parsePhoneNumberFromString(control.value)) {
-        if (parsePhoneNumberFromString(control.value).isValid()) {
-          return null;
+      if (parsePhoneNumberFromString(control.value).isValid()) {
+        return null;
       }
     }
     return ({ 'phoneNumberValid': true });
@@ -17,53 +17,57 @@ function phoneNumberValidator(control: AbstractControl): ValidationErrors | null
 }
 
 @Component({
-    selector: 'intl-tel-input',
-    templateUrl: './index.html',
-    styleUrls: ['./style.scss'],
-  })
-  export class IntlTelInputComponent extends ErrorMessageProvider implements OnInit {
+  selector: 'intl-tel-input',
+  templateUrl: './index.html',
+  styleUrls: ['./style.scss']
+})
+export class IntlTelInputComponent extends ErrorMessageProvider implements OnInit {
 
-    @Input() maxLength;
+  @Input()
+  maxLength;
 
-    @Input() onlyNumbers;
+  @Input()
+  onlyNumbers;
 
-    @Input('user') user: ApiUserObject;
+  @Input('user') user: ApiUserObject;
 
-    @Output()
-    inputUnfocus = new EventEmitter<any>();
+  @Output()
+  inputUnfocus = new EventEmitter<any>();
 
-    AYTPhone: any;
-    phoneNumber: string;
+  AYTPhone: any;
+  phoneNumber: string;
 
-    form = {phone: new FormControl('', [phoneNumberValidator])};
+  form;
 
-    ngOnInit() {
-      if (this.user) {
-        if (this.user.phone) {
-          this.phoneNumber = new AsYouType().input('+' + this.user.countryCallingCode + this.user.phone);
-        }
+  ngOnInit() {
+    this.form = { phone: new FormControl('', [phoneNumberValidator]) };
+
+    if (this.user) {
+      if (this.user.phone) {
+        this.phoneNumber = new AsYouType().input('+' + this.user.countryCallingCode + this.user.phone);
       }
     }
+  }
 
-    onInputChange(phoneInput: string) {
-      this.AYTPhone = new AsYouType().input(phoneInput);
-      log.debug(this.AYTPhone);
-      if (phoneInput) {
-        if (parsePhoneNumberFromString(this.AYTPhone)) {
-          if (parsePhoneNumberFromString(this.AYTPhone).isValid()) {
-            this.inputUnfocus.emit(parsePhoneNumberFromString(this.AYTPhone));
-          } else {
-            this.inputUnfocus.emit('Phone not valid');
-          }
+  onInputChange(phoneInput: string) {
+    this.AYTPhone = new AsYouType().input(phoneInput);
+    log.debug(this.AYTPhone);
+    if (phoneInput) {
+      if (parsePhoneNumberFromString(this.AYTPhone)) {
+        if (parsePhoneNumberFromString(this.AYTPhone).isValid()) {
+          this.inputUnfocus.emit(parsePhoneNumberFromString(this.AYTPhone));
         } else {
           this.inputUnfocus.emit('Phone not valid');
         }
       } else {
-        this.inputUnfocus.emit(null);
+        this.inputUnfocus.emit('Phone not valid');
       }
-    }
-
-    onFocusOut() {
-      this.phoneNumber = this.AYTPhone;
+    } else {
+      this.inputUnfocus.emit(null);
     }
   }
+
+  onFocusOut() {
+    this.phoneNumber = this.AYTPhone;
+  }
+}
