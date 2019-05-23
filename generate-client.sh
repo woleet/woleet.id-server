92a676a47ce93fd2fe724f77ed5302a781e45013
vbarat <vincent.barat@woleet.com>
2019-05-23 17:42:10 +0200

    generate-client.sh now clean generated code at each generation

--- generate-client.sh
+++ generate-client.sh
@@ -5,5 +5,7 @@
 OPENAPI_CODEGEN='java -jar openapi-generator-cli.jar'
 
 # Generation of the Java client used for tests
+rm -rf tests/java/src/main
+rm -rf tests/java/docs
 $OPENAPI_CODEGEN generate -i swagger.yaml -g java -D hideGenerationTimestamp=true -Djava8=true -DdateLibrary=java8 -o tests/java --api-package io.woleet.idserver.api --artifact-id woleet-idserver-api-client --group-id io.woleet.idserver --model-package io.woleet.idserver.api.model --artifact-version 1.0.0
 
