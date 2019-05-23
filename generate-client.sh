b59e84954313ff3d24b78e331d64d12b6e202217
vbarat <vincent.barat@woleet.com>
2019-05-23 17:36:29 +0200

    WOLEET-1072: Fix issue in java tests OpenAPI Generator 3.3.4 used.

--- generate-client.sh
+++ generate-client.sh
@@ -2,7 +2,7 @@
 # Use this script to generate Java test/client code from Woleet.ID Server OpenAPI documentation.
 # ==============================================================================================
 
-OPENAPI_CODEGEN='openapi-generator'
+OPENAPI_CODEGEN='java -jar openapi-generator-cli.jar'
 
 # Generation of the Java client used for tests
 $OPENAPI_CODEGEN generate -i swagger.yaml -g java -D hideGenerationTimestamp=true -Djava8=true -DdateLibrary=java8 -o tests/java --api-package io.woleet.idserver.api --artifact-id woleet-idserver-api-client --group-id io.woleet.idserver --model-package io.woleet.idserver.api.model --artifact-version 1.0.0
