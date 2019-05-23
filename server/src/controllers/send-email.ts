44fb07267ed57fe85664704ebe0fc05675a1faa1
Baptiste <baptiste.legeron@woleet.com>
2019-05-23 14:18:09 +0200

    switch SMTP mode

--- server/src/controllers/send-email.ts
+++ server/src/controllers/send-email.ts
@@ -7,7 +7,7 @@ import { getTransporter } from './smtp';
 import { getServerConfig } from './server-config';
 import { createEnrollment } from './enrollment';
 import log = require('loglevel');
-import * as nodemailer from 'nodemailer';
+// import * as nodemailer from 'nodemailer';
 import { readFileSync } from 'fs';
 import * as path from 'path';
 
@@ -110,25 +110,25 @@ export async function sendEmail(email: string, subject: string, html: any) {
 
   // with ethereal. Catch the email with the url sent in the console.
 
-  const account = await nodemailer.createTestAccount();
+  // const account = await nodemailer.createTestAccount();
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
-    tls: {
-      // do not fail on invalid certs
-      rejectUnauthorized: false
-    },
-    auth: {
-      user: account.user, // generated ethereal user
-      pass: account.pass  // generated ethereal password
-    }
-  });
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.ethereal.email',
+  //   port: 587,
+  //   secure: false,
+  //   tls: {
+  //     // do not fail on invalid certs
+  //     rejectUnauthorized: false
+  //   },
+  //   auth: {
+  //     user: account.user, // generated ethereal user
+  //     pass: account.pass  // generated ethereal password
+  //   }
+  // });
 
   // with configurated SMTP server
 
-  // const transporter = getTransporter();
+  const transporter = getTransporter();
 
   await transporter.sendMail(MailTemplate(email, subject, html), function (err, info) {
     if (err) {
@@ -137,7 +137,7 @@ export async function sendEmail(email: string, subject: string, html: any) {
       log.info(info);
       log.info('Message sent: %s', info.messageId);
       // Preview only available when sending through an Ethereal account
-      log.info('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      // log.info('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     }
   });
 }
