12352c2de56c5ad4aa641b8cbe3eb2ab160c73bc
Pierre LE CORRE <pierre.lecorre.free@gmail.com>
2019-06-24 17:59:41 +0200

    WOLEET-1131 Install docker and compose working

--- app.sh
+++ app.sh
@@ -2,25 +2,32 @@
 
 set -e
 
+CLIENT_IMAGE="${WOLEET_ID_SERVER_REGISTRY:-wids}/client:${WOLEET_ID_SERVER_VERSION:-latest}"
+SERVER_IMAGE="${WOLEET_ID_SERVER_REGISTRY:-wids}/server:${WOLEET_ID_SERVER_VERSION:-latest}"
+
 display_usage_app() {
   echo "usage: $0 [start|stop|restart|build|push|check|logs|backup|restore]"
 }
 
 start() {
-  local old_server=$(docker ps | grep woleetid-server_wid-server_1 | cut -d' ' -f 1)
+  local old_server
+  old_server=$(docker ps | grep woleetid-server_wid-server_1 | cut -d' ' -f 1)
   docker-compose up -d
 
   # If WOLEET_ID_SERVER_ENCRYPTION_SECRET it not set, attaching to the server's container to enter it via CLI
-  if [ -z $WOLEET_ID_SERVER_ENCRYPTION_SECRET ]; then
-    local server=$(docker ps | grep woleetid-server_wid-server_1 | cut -d' ' -f 1)
+  if [[ -z "$WOLEET_ID_SERVER_ENCRYPTION_SECRET" ]
+  then
+    local server
+    server=$(docker ps | grep woleetid-server_wid-server_1 | cut -d' ' -f 1)
 
-    if [ "$server" == "$old_server" ]; then
+    if [ "$server" == "$old_server" ]
+    then
       echo "Server was already running, nothing to do."
       exit 0
     fi
 
     echo "No WOLEET_ID_SERVER_ENCRYPTION_SECRET environment set, attaching to container ${server}..."
-    docker attach $server --detach-keys='ctrl-c'
+    docker attach "$server" --detach-keys='ctrl-c'
   fi
 }
 
@@ -32,7 +39,8 @@ backup() {
   fi
 
   BACKUP_PATH="$1"
-  if [[ -d $BACKUP_PATH ]]; then
+  if [[ -d $BACKUP_PATH ]]
+  then
     echo "Create dump_$(date +%Y-%m-%d_%H_%M_%S).sql in $BACKUP_PATH."
     docker exec woleetid-server_wid-postgres_1 pg_dumpall -c -U postgres -h /var/run/postgresql >"$BACKUP_PATH/dump_$(date +%Y-%m-%d_%H_%M_%S).sql"
   else
@@ -48,14 +56,16 @@ restore() {
   fi
 
   RESTORE_FILE="$1"
-  if [[ ${RESTORE_FILE##*.} == sql ]]; then
+  if [[ ${RESTORE_FILE##*.} == sql ]]
+  then
     docker exec woleetid-server_wid-postgres_1 psql -U postgres -h /var/run/postgresql -c "REVOKE CONNECT ON DATABASE wid FROM ${WOLEET_ID_SERVER_POSTGRES_USER:-pguser};"
     docker exec woleetid-server_wid-postgres_1 psql -U postgres -h /var/run/postgresql -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '${WOLEET_ID_SERVER_POSTGRES_DB:-wid}' AND pid <> pg_backend_pid();"
     docker exec -i woleetid-server_wid-postgres_1 psql -U postgres -h /var/run/postgresql < "$RESTORE_FILE"
     docker exec woleetid-server_wid-postgres_1 psql -U postgres -h /var/run/postgresql -c "GRANT CONNECT ON DATABASE wid TO ${WOLEET_ID_SERVER_POSTGRES_USER:-pguser};"
     if [[ $(docker ps -q --filter name=woleetid-server_wid-server_1 --filter status=running | wc -w) -eq 1 ]]; then
       docker restart woleetid-server_wid-server_1
-      if [ -z $WOLEET_ID_SERVER_ENCRYPTION_SECRET ]; then
+      if [[ -z "$WOLEET_ID_SERVER_ENCRYPTION_SECRET" ]]
+      then
         echo "No WOLEET_ID_SERVER_ENCRYPTION_SECRET environment set, attaching to container ${server}..."
         docker attach woleetid-server_wid-server_1 --detach-keys='ctrl-c'
       fi
@@ -67,43 +77,52 @@ restore() {
 
 operation=$1
 
-if [ -z $operation ]; then
+if [[ -z "$operation" ]]
+then
   display_usage_app
   exit 1
 fi
 
 shift
 
-if [ "$operation" == "start" ]; then
+if [[ "$operation" == "start" ]]
+then
   start
-elif [ "$operation" == "logs" ]; then
+elif [[ "$operation" == "logs" ]]
+then
   docker-compose logs -f --tail 50
-elif [ "$operation" == "stop" ]; then
+elif [[ "$operation" == "stop" ]]
+then
   docker-compose down
-elif [ "$operation" == "restart" ]; then
+elif [[ "$operation" == "restart" ]]
+then
   docker-compose down
   start
-elif [ "$operation" == "push" ]; then
-  docker-compose push
-elif [ "$operation" == "check" ]; then
+elif [[ "$operation" == "push" ]]
+then
+  docker push "$CLIENT_IMAGE"
+  docker push "$SERVER_IMAGE"
+elif [[ "$operation" == "check" ]]
+then
   cd server
   npm run lint
   npm run check
   cd ../client
   npm run lint
-elif [ "$operation" == "build" ]; then
-  registry=${WOLEET_ID_SERVER_REGISTRY:-woleet-id-server}
-
-  echo "Build client image (${registry}:client)..."
-  docker build -f Dockerfile.client -t "${registry}:client" .
+elif [[ "$operation" == "build" ]]
+then
+  echo "Build client image ($CLIENT_IMAGE)..."
+  docker build -f Dockerfile.client -t "$CLIENT_IMAGE" .
   echo "Done."
 
-  echo "Building server image (${registry}:server)..."
-  docker build -f Dockerfile.server -t "${registry}:server" .
+  echo "Building server image ($SERVER_IMAGE)..."
+  docker build -f Dockerfile.server -t "$SERVER_IMAGE" .
   echo "Done."
-elif [ "$operation" == "backup" ]; then
+elif [[ "$operation" == "backup" ]]
+then
   backup "$@"
-elif [ "$operation" == "restore" ]; then
+elif [[ "$operation" == "restore" ]]
+then
   restore "$@"
 else
   display_usage_app
---
+++ onlineSetup.sh
@@ -0,0 +1,259 @@
+#!/usr/bin/env bash
+
+set -e
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+get_distribution() {
+  lsb_dist=""
+  # Every system that we officially support has /etc/os-release
+  if [ -r /etc/os-release ]
+  then
+    lsb_dist="$(. /etc/os-release && echo "$ID")"
+  fi
+  # Returning an empty string here should be alright since the
+  # case statements don't act unless you provide an actual value
+  echo "$lsb_dist"
+}
+
+isVersionOK() {
+  currentVersion=$(echo "$1" | grep -oE '([[:digit:]]\.?)+' )
+  minimalVersion=$(echo "$2" | grep -oE '([[:digit:]]\.?)+' )
+
+  if [ -z "$currentVersion" ]
+  then
+    return 1
+  elif [ -z "$minimalVersion" ]
+  then
+    return 1
+  fi
+
+  if [ "$currentVersion" == "$minimalVersion" ]
+  then
+    return 0
+  elif [ "$(printf "%s\n" "$currentVersion" "$minimalVersion" | sort -V | tail -n 1 )" == "$minimalVersion" ]
+  then
+    return 1
+  fi
+  return 0
+}
+
+setShC() {
+  user="$(id -un 2>/dev/null || true)"
+
+  sh_c='sh -c'
+  if [ "$user" != 'root' ]
+  then
+    if command_exists sudo
+    then
+      sh_c='sudo -E sh -c'
+    elif command_exists su
+    then
+      sh_c='su -c'
+    else
+  printf '%s\n' '
+Error: this installer needs the ability to run commands as root.
+We are unable to find either "sudo" or "su" available to make this happen.'
+      exit 1
+    fi
+  fi
+}
+
+installDocker() {
+  # perform some very rudimentary platform detection
+  lsb_dist=""
+  # Every system that we officially support has /etc/os-release
+  if [ -r /etc/os-release ]
+  then
+    lsb_dist="$(. /etc/os-release && echo "$ID" | tr '[:upper:]' '[:lower:]' )"
+  fi
+
+  # Run setup for each distro accordingly
+  case "$lsb_dist" in
+    ubuntu|debian)
+      if [ "$lsb_dist" = "ubuntu" ]
+      then
+        if command_exists lsb_release
+        then
+          dist_version="$(lsb_release --codename | cut -f2)"
+        fi
+        if [ -z "$dist_version" ] && [ -r /etc/lsb-release ]
+        then
+          dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
+        fi
+      fi
+
+      if [ "$lsb_dist" = "debian" ]
+      then
+        dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
+        case "$dist_version" in
+          9)
+            dist_version="stretch"
+          ;;
+          8)
+            dist_version="jessie"
+          ;;
+        esac
+      fi
+
+      pre_reqs="apt-transport-https ca-certificates"
+      if ! command -v gpg > /dev/null
+      then
+        pre_reqs="$pre_reqs gnupg"
+      fi
+      apt_repo="deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$lsb_dist $dist_version stable"
+      (
+        set -x
+        $sh_c 'apt-get update -qq >/dev/null'
+        $sh_c "apt-get install -y -qq $pre_reqs >/dev/null"
+        $sh_c "curl -fsSL \"https://download.docker.com/linux/$lsb_dist/gpg\" | apt-key add -qq - >/dev/null"
+        $sh_c "echo \"$apt_repo\" > /etc/apt/sources.list.d/docker.list"
+        $sh_c 'apt-get update -qq >/dev/null'
+        $sh_c "apt-get install -y -qq --no-install-recommends docker-ce >/dev/null"
+      )
+      ;;
+    centos|fedora)
+      yum_repo="https://download.docker.com/linux/$lsb_dist/docker-ce.repo"
+      if ! curl -Ifs "$yum_repo" > /dev/null
+      then
+        echo "Error: Unable to curl repository file $yum_repo, is it valid?"
+        exit 1
+      fi
+      if [ "$lsb_dist" = "fedora" ]
+      then
+        pkg_manager="dnf"
+        config_manager="dnf config-manager"
+        enable_channel_flag="--set-enabled"
+        disable_channel_flag="--set-disabled"
+        pre_reqs="dnf-plugins-core"
+      else
+        pkg_manager="yum"
+        config_manager="yum-config-manager"
+        enable_channel_flag="--enable"
+        disable_channel_flag="--disable"
+        pre_reqs="yum-utils"
+      fi
+      (
+        set -x
+        $sh_c "$pkg_manager install -y -q $pre_reqs"
+        $sh_c "$config_manager --add-repo $yum_repo"
+
+        $sh_c "$config_manager $disable_channel_flag docker-ce-*"
+        $sh_c "$config_manager $enable_channel_flag docker-ce-stable"
+
+        $sh_c "$pkg_manager makecache"
+        $sh_c "$pkg_manager install -y -q docker-ce"
+      )
+      ;;
+      *)
+        echo "this script is not available on this OS"
+        exit 1
+      ;;
+  esac
+}
+
+startEnableDocker() {
+  (
+    set -x
+    $sh_c 'systemctl daemon-reload; systemctl start docker; systemctl enable docker'
+  )
+}
+
+installDockerCompose() {
+  (
+    set -x
+    $sh_c "curl -fsSL \"https://github.com/docker/compose/releases/download/1.24.0/docker-compose-Linux-x86_64\" -o /usr/local/bin/docker-compose"
+    $sh_c "chmod +x /usr/local/bin/docker-compose"
+  )
+}
+
+installWids() {
+  install_dir="${HOME}/wids"
+  echo "Wids will be installed in $install_dir"
+  if [ ! -d "$install_dir" ]
+  then
+    git clone https://github.com/woleet/woleet.id-server.git "$install_dir"
+    cd "$install_dir"
+  else
+    cd "$install_dir"
+    git fetch
+  fi
+  LATEST_TAG=$(git tag | grep -E '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | sort | tail -n 1)
+  git checkout "$LATEST_TAG"
+}
+
+install() {
+  if [ "$(uname -m)" != 'x86_64' ]
+  then
+    echo "WIDS is not available on this architecture"
+    exit 1
+  fi
+
+  setShC
+
+  if ! command_exists bash
+  then
+    echo "Please install git before rerunning this scipt"
+    exit 1
+  fi
+
+  if ! command_exists curl
+  then
+    echo "Please install curl before rerunning this scipt"
+    exit 1
+  fi
+
+  if ! command_exists git
+  then
+    echo "Please install git before rerunning this scipt"
+    exit 1
+  fi
+
+  if ! command_exists docker
+  then
+    installDocker
+    startEnableDocker
+    (
+      set -x
+      $sh_c "usermod -aG docker $USER"
+    )
+    # TODO (Creates a new shell T_T)
+    # newgrp docker
+  elif ! isVersionOK "$(docker version --format '{{.Server.Version}}' | grep -E '([[:digit:]]\.?)+' )" "17.09.0"
+  then
+    echo "Please upgrade your docker version to at least 17.09.0+ before rerunning this script"
+    exit 1
+  fi
+
+  if ! command_exists docker-compose
+  then
+    installDockerCompose
+  elif ! isVersionOK "$(docker-compose version --short)" "1.17"
+  then
+    echo "Please upgrade your docker-compose version to at least 1.17+ before rerunning this script"
+    exit 1
+  fi
+
+  exit 0
+
+  installWids
+  # Configure
+  export WOLEET_ID_SERVER_ENCRYPTION_SECRET='secret'
+  #export WOLEET_ID_SERVER_DATA_DIR
+  export WOLEET_ID_SERVER_HTTP_TLS_CERTIFICATE="$HOME/ssl/woleet.crt"
+  export WOLEET_ID_SERVER_HTTP_TLS_KEY="$HOME/ssl/woleet.key"
+  export WOLEET_ID_SERVER_VERSION="latest"
+  # Start
+  (
+    set -x
+    $sh_c "./app.sh start"
+  )
+
+  echo "You will have to log out and back to be able to interact with app.sh and docker"
+}
+
+# wrapped up in a function so that we have some protection against only getting
+# half the file during "curl | sh"
+install
