18ae85616b63a392f448e9a8dd32b828d59b8860
Baptiste <baptiste.legeron@woleet.com>
2019-07-17 17:22:06 +0200

    Bug correction on Mail template

--- server/src/controllers/send-email.ts
+++ server/src/controllers/send-email.ts
@@ -139,9 +139,10 @@ export async function sendEmail(email: string, subject: string, html: any) {
  */
 function MailTemplate(email: string, subject: string, html: any): object {
   const organizationName = getServerConfig().organizationName;
+  const contactDomain = getServerConfig().contact ?
+  getServerConfig().contact.split('@')[1] : 'Woleet';
   return {
-    from: organizationName + ' no-reply@' + getServerConfig().contact ?
-      getServerConfig().contact.split('@')[1] : 'Woleet',
+    from: organizationName + ' no-reply@' + contactDomain,
     to: email,
     subject: subject,
     html: html
