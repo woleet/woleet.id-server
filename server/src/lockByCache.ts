b47bd5cc41512ed947be0b572cb6d72546215028
Pierre LE CORRE <pierre.lecorre.free@gmail.com>
2020-09-16 15:43:13 +0200

    WOLEET-1790 Cleanup lockByCache

--- server/src/lockByCache.ts
+++ server/src/lockByCache.ts
@@ -1,10 +1,11 @@
-import { Sequelize } from "sequelize";
-import { cacheConfig } from "./config";
+import { cacheConfig } from './config';
 import { exit } from './exit';
 import * as log from 'loglevel';
 import * as Redis from 'ioredis';
 import * as Redlock from 'redlock';
 
+// This function is to be used when a function must be run only one at time in a cluster environnement
+// when there is only one server (redis not configured) this function does not lock anything
 export async function doLockByCache(lockName: string, functionToCall: () => any) {
   switch (cacheConfig.type) {
     case 'local': {
@@ -13,9 +14,10 @@ export async function doLockByCache(lockName: string, functionToCall: () => any)
     }
     case 'redis': {
       const redis = new Redis(cacheConfig.port, cacheConfig.host);
+      // Here redlock is configured to retry indefinitly when trying to get a lock.
       const redlock = new Redlock([redis], {retryCount: -1});
       log.info(`Trying to take redis lock: ${lockName}`);
-      const lock = await redlock.lock(lockName, 5 * 60 * 1000); // TTL is 5 minutes
+      const lock = await redlock.lock(lockName, 5 * 60 * 1000); // TTL is 5 minutes,  after this delay this lock will be destroyed
       await functionToCall();
       log.info(`Releasing redis lock: ${lockName}`);
       lock.unlock();
