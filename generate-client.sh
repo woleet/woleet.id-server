8ef0358ea21a6341dbb22d046b5e668f4e6e0d99
vbarat <vincent.barat@woleet.com>
2021-08-04 11:46:50 +0200

    WOLEET-2192: Fix vulns + code generation harmonization with Woleet API

--- generate-client.sh
+++ generate-client.sh
@@ -12,10 +12,4 @@ rm -rf tests/java/docs
 export JAVA_OPTS='-Djava8=true -DmodelTests=false -DdateLibrary=java8'
 
 # Generation of the Java client used for tests
-$OPENAPI_CODEGEN generate -i swagger.yaml -g java -p hideGenerationTimestamp=true -o tests/java --api-package io.woleet.idserver.api --artifact-id woleet-idserver-api-client --group-id io.woleet.idserver --model-package io.woleet.idserver.api.model --artifact-version 1.0.0
-
-# Clean unused generated code
-rm -rf tests/java/*gradle*
-rm -rf tests/java/target
-rm -rf tests/java/build.*
-rm -rf tests/java/git_push.sh
+$OPENAPI_CODEGEN generate -p hideGenerationTimestamp=true -i openapi.yaml -g java -o tests/java --api-package io.woleet.idserver.api --artifact-id woleet-idserver-api-client --group-id io.woleet.idserver --model-package io.woleet.idserver.api.model --artifact-version 1.0.0
